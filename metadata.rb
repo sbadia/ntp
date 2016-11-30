7f457c6617a0cdd970dce756c4d9472b9e9028f6
Sebastien Badia <seb@sebian.fr>
2016-11-30 15:40:44 +0100

    metadata: Just remove windows dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,15 +4,13 @@ maintainer_email 'gmiranda@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures ntp as a client or server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.10.1'
+version '1.10.2'
 
 recipe 'ntp', 'Installs and configures ntp either as a server or client'
 
 %w( amazon centos debian fedora freebsd gentoo redhat scientific solaris2 oracle ubuntu windows xcp ).each do |os|
   supports os
 end
-
-depends 'windows'
 
 source_url 'https://github.com/gmiranda23/ntp' if respond_to?(:source_url)
 issues_url 'https://github.com/gmiranda23/ntp/issues' if respond_to?(:issues_url)
